2fe01229611d285395f4d41ce0814481354dcf4c
Someshwar Rao <123357802+CodeWithSomesh@users.noreply.github.com>
2023-03-15 19:45:20 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,75 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
+const client = require("@mailchimp/mailchimp_marketing");
+
+const app = express();
+
+client.setConfig({apiKey: "ed0fd1e9f7d7b685c8bcae39f2f09f79-us11",  server: "us11"});
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res){
+
+    res.sendFile(__dirname + "/signup.html")
+
+});
+
+app.post("/", function(req, res){
+
+    const firstName = req.body.fname;
+    const lastName = req.body.lname;
+    const email = req.body.email;
+
+    const apiKey = "ed0fd1e9f7d7b685c8bcae39f2f09f79-us11";
+    const listID = "555e14bff0";
+
+    const subscribedUser = {
+
+        firstName: firstName, 
+        lastName: lastName,
+        email: email
+
+    }
+
+    const run = async () => {
+        const response = await client.lists.addListMember(listID, {
+          email_address: subscribedUser.email,
+          status: "subscribed",
+          merge_fields: {
+              FNAME: subscribedUser.firstName,
+              LNAME: subscribedUser.lastName
+          }
+        });
+    };
+
+    //run();
+
+    if(res.statusCode === 200){
+
+        res.sendFile(__dirname + "/success.html");
+        console.log("Succesfully added contact as an audience member.");
+
+    }
+    else {
+
+        res.sendFile(__dirname + "/failure.html");
+
+    };
+
+});
+
+
+app.post("/failure", function(req, res){
+
+    res.redirect("/");
+
+});
+
+app.listen(process.env.PORT || 3000, function(){
+
+    console.log("Server is running on port 3000.");
+
+});
